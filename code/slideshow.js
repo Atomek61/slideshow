90dad6e31c5a746d4a94ac564f1b8049c2543be8
Jan Schirrmacher <jan@atomek.de>
2022-10-28 08:49:42 +0200

    Online documentation, more examples, fullscreen, autoStop

--- code/slideshow.js
+++ code/slideshow.js
@@ -20,13 +20,14 @@
 //   Insert a div section in your <body> like
 //   <div id="myslideshowid"></div>
 //
-//   Create a Slideshow instance and initilize it with
+//   Create a Slideshow instance and initialize it with
 //   the Id of your div and a list of pictures with url and title.
 //   let slideshow = new Slideshow("myslideshowid", [img1.jpg, "Image 1", img2.jpg, "Image 2"]);
 //
 // Revision History 
 // V1.00 JS 2022-10-22 Initial release
 // V1.10 JS 2022-10-23 Added cinema mode, renamed image to picture
+// V1.20 JS 2022-10-28 Added autoStop, more properties instead of methods, css improvements, fullscreen-support
 
 // helper to enable % for negative numbers
 function mod(n, m) {
@@ -37,37 +38,38 @@ function mod(n, m) {
 const SLIDESHOWTEMPLATE
   = '<div class="title"></div>\r\n'
   + '<div class="pictures">\r\n'
-  + '%IMAGES'
+  + '  %IMAGES'
+  + '</div>\r\n'
   + '<span class="prev symbol"></span>\r\n'
   + '<span class="next symbol"></span>\r\n'
-  + '</div>\r\n'
   + '<div class="bullets">\r\n'
-  + '%BULLETS'
-  + '<span class="cinema symbol"></span>\r\n'
+  + '  %BULLETS'+'<span class="cinema symbol"></span>\r\n'
   + '</div>\r\n';
 
 const IMGTEMPLATE = '<img class="picture" src="%URL" data-title="%TITLE"/>\r\n';
 
 const BULLETTEMPLATE  = '<span class="bullet symbol"></span>\r\n';
 
+const DEFAULTCINEMAINTERVAL = 4000;
+const MINCINEMAINTERVAL = 100;
+
 // Main class - instantiate for a slide show
 class Slideshow {
 
-// containerId is the Id of a div or body
+// container is the is the element (block) which receives the slideshow.
 // pictureList is an optional string array [url1, title1, url2, title2, .... ] with pairs url/title
-// if slide show operates on an emtpy container tag, create such a list.
-  constructor(containerId, picturelist = null) {
-    this.containerId = containerId;
-    this.container = document.getElementById(containerId);
-    this.cinemaInterval = 4000;
+// if there is no pictureList, slideshow assumes img tags with data-title.
+  constructor(container, picturelist = null) {
+    this.container = container;
+    this.imageIndex_ = -1;
+    this.cinemaInterval_ = DEFAULTCINEMAINTERVAL;
+    this.cinemaTimerId = -1;
     this.isCinema = false;
-    this.cinemaIntervalId = -1;
-    this.currentIndex = -1;
-    this.container.classList.add("slideshow");
+    this.autoStop = true;
+    this.container.classList.add("container");
     if (picturelist) {
       this.container.innerHTML = ""; 
       let pictureshtml = "";
-      this.currentIndex = -1;
       this.pictureCount = picturelist.length/2;          
       for (let i=0; i<this.pictureCount; i++)
         pictureshtml += IMGTEMPLATE.replace(/%URL/g, picturelist[i*2]).replace(/%TITLE/g, picturelist[i*2+1]);
@@ -75,7 +77,6 @@ class Slideshow {
       for (let i=0; i<this.pictureCount; i++)
         bulletshtml += BULLETTEMPLATE.replace(/%INDEX/g, (i).toString());
       this.container.innerHTML = SLIDESHOWTEMPLATE
-        .replace(/%ID/g, this.containerId)
         .replace(/%IMAGES/g, pictureshtml)
         .replace(/%BULLETS/g, bulletshtml);
     }  
@@ -84,8 +85,8 @@ class Slideshow {
     for (let i=0; i<bullets.length; i++) {
       bullets[i].slideshow = this;
       bullets[i].addEventListener("click", (event) => {
-        this.stop();
-        this.showPicture(i);
+        if (this.autoStop) this.stop();
+        this.imageIndex = i;
       });
     }
     
@@ -102,63 +103,80 @@ class Slideshow {
 
     let btn = this.container.querySelector(".next");
     if (btn) btn.addEventListener("click", (event)=>{
-      this.stop();
-      this.nextPicture(1);
+      if (this.autoStop) this.stop();
+      this.imageIndex += 1;
     });
 
     btn = this.container.querySelector(".prev");
     if (btn) btn.addEventListener("click", (event)=>{
-      this.stop();
-      this.nextPicture(-1);
+      if (this.autoStop) this.stop();
+      this.imageIndex -= 1;
     });
-    this.showPicture(0);
+    this.imageIndex = 0;
   }
 
-  // show a certain picture by index
-  showPicture(index) {
-    index = mod(index, this.pictureCount);
-    if (index!=this.currentIndex) {
+  get imageIndex() {
+    return this.imageIndex_;
+  }
+
+  set imageIndex(value) {
+    value = mod(value, this.pictureCount);
+    if (value!=this.imageIndex_) {
       let pictures = this.container.getElementsByTagName("img");
       let bullets = this.container.getElementsByClassName("bullet");
-      if (this.currentIndex!=-1) {
-        pictures[this.currentIndex].style.display = "none";
-        bullets[this.currentIndex].classList.remove("bullet-active");
+      if (this.imageIndex_!=-1) {
+        pictures[this.imageIndex_].style.display = "none";
+        bullets[this.imageIndex_].classList.remove("bullet-active");
       }              
-      this.currentIndex = index;
+      this.imageIndex_ = value;
       let title = this.container.querySelector(".title");
-      title.innerHTML = pictures[this.currentIndex].dataset.title;
-      pictures[this.currentIndex].style.display = "block";
-      bullets[this.currentIndex].classList.add("bullet-active");
+      title.innerHTML = pictures[this.imageIndex_].dataset.title;
+      pictures[this.imageIndex_].style.display = "block";
+      bullets[this.imageIndex_].classList.add("bullet-active");
     }
   }
 
-  // for prev and next buttons
-  nextPicture(offset) {
-    this.showPicture(this.currentIndex + offset);
+  get cinemaInterval() {
+    return this.cinemaInterval_;
+  }
+
+  set cinemaInterval(value) {
+    if (value!=this.cinemaInterval_) {
+      if (value<MINCINEMAINTERVAL) value=MINCINEMAINTERVAL;
+      this.cinemaInterval_ = value;
+      if (this.isCinema) {
+        this.stop();
+        this.start();
+      }
+    }
   }
 
   start(immediate = false) {
     if (!this.isCinema) {
-      this.cinemaIntervalId = setInterval(() => {
-        this.nextPicture(1);
-      }, this.cinemaInterval);
+      this.cinemaTimerId = setInterval(() => {
+        this.imageIndex += 1;
+      }, this.cinemaInterval_);
       let cinemaButtons = this.container.getElementsByClassName("cinema");
       if (cinemaButtons.length>0)
         cinemaButtons[0].classList.add("cinema-playing");
       if (immediate)
-        this.nextPicture(1);
+        this.imageIndex = 1;
       this.isCinema = true;
     }
   }
 
   stop() {
     if (this.isCinema) {
-      clearInterval(this.cinemaIntervalId);
+      clearInterval(this.cinemaTimerId);
       let cinemaButtons = this.container.getElementsByClassName("cinema");
       if (cinemaButtons.length>0)
         cinemaButtons[0].classList.remove("cinema-playing");
       this.isCinema = false;
     }
   }
+
+  speed(factor = 1.5) {
+    this.cinemaInterval /= factor;
+  }  
 
 }
